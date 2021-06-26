22751b1a5bcb10997dc5ac53d16a2267bba11a38
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-06-26 17:50:54 +0600

    map concept

--- map-filter.js
+++ map-filter.js
@@ -1,5 +1,7 @@
 const number = [1,3,5,7,9];
 const output = [];
+
+
 // for (let i=0; i< number.length;i++){
 
 //     const element = number[i];
@@ -11,6 +13,30 @@ const output = [];
 // console.log("output",output);
 
 
-number.map(function(element,index,array){
-    console.log(element,index,array)
-})
+
+// number.map(function(element,index,array){
+//     console.log(element,index,array)
+// })
+
+
+// const square = element =>element*element;
+
+
+
+// const result = number.map(function(element){
+//     return element * element;
+// })
+
+const value =number.map(x=> x);
+console.log(value);
+
+const result =number.map(x=> x*x);
+console.log(result);
+
+
+const bigger =number.filter(x=> x >5);
+console.log(bigger );
+
+
+const findvalue =number.find(x=> x >5);
+console.log(findvalue);
