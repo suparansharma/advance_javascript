6dc88fbd5644275639c25d3a61143f0fcf7a928d
suparansharma <49103115+suparansharma@users.noreply.github.com>
2021-06-30 14:29:54 +0600

    object

--- object-properties.js
+++ object-properties.js
@@ -6,12 +6,35 @@ const students = [
     {id: 51, name:'Rony sunny'},
 ]
 
+
+const teacher = [
+
+    {id: 21, name:'omer sunny'},
+    {id: 31, name:'Karim sunny'},
+    {id: 41, name:'Rahim sunny'},
+    {id: 51, name:'Rony sunny'},
+]
+
 const name = students.map(s => s.name);
 const id = students.map(s=> s.id);
 const bigger = students.filter(s=> s.id>31);
 const finde = students.find(s=> s.id>31);
 
+
+const tname = teacher.map(s => s.name);
+const tid = teacher.map(s=> s.id);
+const tbigger = teacher.filter(s=> s.id>31);
+const tfinde = teacher.find(s=> s.id>31);
+
+
+
 console.log(name);
 console.log(id);
 console.log(bigger);
 console.log(finde);
+
+
+console.log(tname);
+console.log(tid);
+console.log(tbigger);
+console.log(tfinde);
