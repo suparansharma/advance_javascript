3ec136134fe83daa4329b7a1be9853aca9d53842
suparansharma <suparansharmap@gmail.com>
2023-02-27 12:53:09 +0600

    leap year condition and object

---
+++ object.js
@@ -0,0 +1,11 @@
+const shoppingChart = {
+
+    shirt:1,
+    pant:2,
+    cap:1,
+    belt:2,
+    shoue:1
+}
+
+console.log(Object.keys(shoppingChart));
+console.log(Object.values(shoppingChart));
