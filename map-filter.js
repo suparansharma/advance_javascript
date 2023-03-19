73edb565696a1f4418124c6a7677d647e09de8bf
suparansharma <suparansharmap@gmail.com>
2023-03-19 21:38:52 +0600

    prev

--- map-filter.js
+++ map-filter.js
@@ -2,14 +2,14 @@ const number = [1,3,5,7,9];
 const output = [];
 
 
-for (let i=0; i< number.length;i++){
+// for (let i=0; i< number.length;i++){
 
-    const element = number[i];
-    const result = element*element;
-    output.push(result);
+//     const element = number[i];
+//     const result = element*element;
+//     output.push(result);
 
 
-}
+// }
 // console.log("output",output);
 
 
